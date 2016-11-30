d524a00d08e0cf36a02a3f92e20aa859fbb5bc22
Juan Pablo <esponja.014@gmail.com>
2016-11-29 18:35:26 -0600

    Initial Commit

---
+++ Prueba1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prueba1
+//
+//  Created by Juan Pablo Martinez Ruiz on 29/11/16.
+//  Copyright © 2016 Pankitech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
